3931a4217c4e38c703a56f04e0eca1bf72a5f992
AydenTFoxx <aydentfox@gmail.com>
2025-08-23 14:19:28 -0300

    Updated target selector class - Changed constant `POSSESSION_RADIUS` from `480` to `720` (it is still not enough) - Added `int` constant `MAX_INPUT_TIME`, with a value of `120` - `creatures` is now initialized at creation, and can no longer be `null` - `Input` setter changed from `public` to `private` - Removed getter methods `GetManager()` and `GetPlayer()` - `ConfirmSelection()` now only sets the player's possession if `Input.InputTime` was not exceeded - Selecting a creature now also freezes the player's input, and releases it if no target was chosen - Reaching the end of `creatures` now wraps the selection target to the other end of the list - Added better checking for resetting the target's possession - Changed `mushroomCounter` from `20` to `10`   - Also no longer overrides a stronger effect (e.g. by eating a Mushroom) - Selecting a creature now causes slugcat to look in its direction - Separated controls logic to new `UpdateClassic()` method - Renamed method `GetDistanceToPosition(Creature, Player)` to `GetDistanceToPlayer(Creature, Player)   - Fixed method not returning the proper distance between both targets - Added null-check safeguard to `GetSortedCreatures(Player)` - Batflies are now of lower priority than all other creatures, and (usually) moved to the end of the list - Updated record `TargetInput`:   - No longer requires any argument; Fields are implicitly initialized as their default values.   - Added field `InputTime`, of type `int`

--- ControlLib/src/Possession/TargetSelector.cs
+++ ControlLib/src/Possession/TargetSelector.cs
@@ -1,54 +1,126 @@
 using System.Collections.Generic;
 using System.Linq;
 using ControlLib.Utils;
-using MoreSlugcats;
 using RWCustom;
+using static ControlLib.ControlLibMain;
 
 namespace ControlLib.Possession;
 
 public sealed class TargetSelector(Player player, PossessionManager manager)
 {
-    private const float POSSESSION_RADIUS = 480f;
+    private const float POSSESSION_RADIUS = 720f;
+    private const int MAX_INPUT_TIME = 120;
 
-    private WeakList<Creature> creatures;
+    private WeakList<Creature> creatures = [];
 
-    public Creature Target { get; private set; }
-    public TargetInput Input { get; set; } = new();
-
-    public PossessionManager GetManager() => manager;
-    public Player GetPlayer() => player;
+    public Creature? Target { get; private set; }
+    public TargetInput Input { get; private set; } = new();
 
     public bool IsTargetValid() => Target is not null && manager.CanPossessCreature(Target);
-    public void SetTargetPossession() => manager.StartPossession(Target);
-    public void ResetTargetPossession() => manager.StopPossession(Target);
+    public void SetTargetPossession() => manager.StartPossession(Target!);
+    public void ResetTargetPossession() => manager.StopPossession(Target!);
+
+    public void ConfirmSelection()
+    {
+        Input = new();
+        creatures.Clear();
+
+        if (IsTargetValid() && Input.InputTime < MAX_INPUT_TIME)
+        {
+            SetTargetPossession();
+        }
+
+        if (Target is null)
+        {
+            manager.UnfreezePlayerControls();
+        }
+    }
+
+    public void SelectNextCreature(int offset, Creature? lastTarget)
+    {
+        Input.IsActive = true;
+
+        if (creatures is { Count: 0 })
+        {
+            creatures = GetSortedCreatures(player);
+        }
+
+        if (!TrySelectNextCreature(lastTarget, offset)) return;
+
+        CLLogger.LogInfo($"Selected creature: {Target}");
+    }
+
+    internal bool TrySelectNextCreature(Creature? lastTarget = null, int offset = 0)
+    {
+        if (creatures is { Count: 0 }) return false;
+
+        int i = lastTarget is not null ? creatures.IndexOf(lastTarget) + offset : offset;
+
+        Target = creatures.ElementAtOrDefault(i);
+        Target ??= (offset > 0) ? creatures.First() : creatures.Last();
+
+        if (!IsTargetValid())
+        {
+            CLLogger.LogWarning($"{Target} is not a valid possession target.");
+
+            creatures.Remove(Target);
+
+            Target = null;
+        }
+
+        return Target is not null;
+    }
 
     public void Update()
     {
-        if (Input.LockAction) return;
+        if (Input.LockAction || Input.InputTime > MAX_INPUT_TIME) return;
 
-        if (manager.IsPossessing)
+        if (Target is not null && manager.HasPossession(Target))
         {
             ResetTargetPossession();
 
             Input.LockAction = true;
             return;
         }
-        else if (manager.PossessionCooldown > 0)
+        else if (!manager.IsPossessing && manager.PossessionCooldown > 0)
         {
             Input.LockAction = true;
             return;
         }
 
-        if (UnityEngine.Random.value < 0.5)
+        Input.InputTime++;
+
+        if (player.mushroomCounter < 10)
         {
-            Target?.room.AddObject(new VoidParticle(Target.mainBodyChunk.pos, Custom.RNV() * 0.5f, 32f));
+            player.mushroomCounter = 10;
         }
 
-        player.mushroomCounter = 20;
+        if (player.controller is null)
+        {
+            manager.FreezePlayerControls();
+        }
 
+        if (Target is not null && player.graphicsModule is PlayerGraphics playerGraphics)
+        {
+            playerGraphics.LookAtObject(Target);
+
+            if (Input.InputTime % 4 == 0)
+            {
+                player.room?.AddObject(new ShockWave(Target.mainBodyChunk.pos, 48f, 0.02f, 4));
+            }
+        }
+
+        if (ClientOptions?.selectionMode == "classic")
+        {
+            UpdateClassicMode();
+        } // TODO: Add "ascension" selection mode
+    }
+
+    public void UpdateClassicMode()
+    {
         if (!Input.IsActive)
         {
-            SelectNextCreature(1);
+            SelectNextCreature(0, null);
             return;
         }
 
@@ -63,69 +135,26 @@ public sealed class TargetSelector(Player player, PossessionManager manager)
         {
             Input.Offset = offset;
 
-            SelectNextCreature(Input.Offset);
+            CLLogger.LogDebug($"Received input! {Input.Offset}");
+
+            SelectNextCreature(Input.Offset, Target);
         }
     }
 
-    public void ConfirmSelection()
-    {
-        Input = new();
-        creatures.Clear();
-
-        if (Target is null)
-        {
-            CLLogger.LogWarning("Tried to possess an invalid creature, ignoring.");
-        }
-        else
-        {
-            SetTargetPossession();
-        }
-    }
-
-    public void SelectNextCreature(int offset)
-    {
-        Input.IsActive = true;
-
-        if (creatures is null or { Count: 0 })
-        {
-            creatures = GetSortedCreatures(player);
-        }
-
-        if (TrySelectNextCreature(lastTarget: Target, offset))
-        {
-            CLLogger.LogInfo($"Selected creature: {Target}");
-        }
-    }
-
-    internal bool TrySelectNextCreature(Creature lastTarget = null, int offset = 0)
-    {
-        if (creatures.Count == 0) return false;
-
-        int i = lastTarget is not null ? creatures.IndexOf(lastTarget) + offset : offset;
-
-        Target = creatures.ElementAtOrDefault(i);
-        Target ??= (offset < 0) ? creatures.First() : creatures.Last();
-
-        if (!IsTargetValid())
-        {
-            CLLogger.LogWarning($"{Target} is not a valid possession target.");
-
-            creatures.Remove(Target);
-
-            Target = null;
-        }
-
-        return Target is not null;
-    }
-
-    private static float GetDistanceToPosition(Creature creature, Player player) =>
-        (creature.mainBodyChunk.pos - player.mainBodyChunk.pos).magnitude;
+    private static float GetDistanceToPlayer(Creature creature, Player player) =>
+        UnityEngine.Vector2.Distance(creature.mainBodyChunk.pos, player.mainBodyChunk.pos);
 
     private static WeakList<Creature> GetSortedCreatures(Player player)
     {
-        List<Creature> sortedCreatures = [.. player.room?.abstractRoom.creatures
+        if (player.room is null)
+        {
+            CLLogger.LogWarning("Player is not in a room; No creature can be queried.");
+            return [];
+        }
+
+        List<Creature> sortedCreatures = [.. player.room.abstractRoom.creatures
             .Select(ac => ac.realizedCreature)
-            .Where(c => c is not (null or Player or { dead: true }) && IsInPossessionRange(player, c))
+            .Where(c => !PossessionManager.IsBannedPossessionTarget(c) && IsInPossessionRange(player, c))
         ];
 
         if (sortedCreatures is null or { Count: 0 })
@@ -136,15 +165,19 @@ public sealed class TargetSelector(Player player, PossessionManager manager)
 
         sortedCreatures.Sort((c1, c2) =>
         {
-            float dist1 = GetDistanceToPosition(c1, player);
-            float dist2 = GetDistanceToPosition(c2, player);
+            if (c1 is Fly && c2 is not Fly) return 1;
+
+            float dist1 = GetDistanceToPlayer(c1, player);
+            float dist2 = GetDistanceToPlayer(c2, player);
 
             return dist1 < dist2
-                ? 1
+                ? -1
                 : dist1 == dist2
                     ? 0
-                    : -1;
+                    : 1;
         });
+
+        CLLogger.LogDebug($"Sorted crits: {PossessionManager.FormatPossessions(sortedCreatures)}");
 
         return [.. sortedCreatures];
     }
@@ -153,14 +186,10 @@ public sealed class TargetSelector(Player player, PossessionManager manager)
         Custom.DistLess(player.mainBodyChunk.pos, creature.mainBodyChunk.pos, POSSESSION_RADIUS);
 }
 
-public record class TargetInput(bool IsActive, bool LockAction, int Offset)
+public record class TargetInput()
 {
-    public TargetInput()
-        : this(false, false, 0)
-    {
-    }
-
-    public bool IsActive { get; set; } = IsActive;
-    public bool LockAction { get; set; } = LockAction;
-    public int Offset { get; set; } = Offset;
+    public bool IsActive { get; set; }
+    public bool LockAction { get; set; }
+    public int Offset { get; set; }
+    public int InputTime { get; set; }
 }
