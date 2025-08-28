109ba75443340a0040ba8987653d07bd123947c0
AydenTFoxx <aydentfox@gmail.com>
2025-08-28 00:48:20 -0300

    Updated compatibility management - Updated class `Utils.CompatibilityManager`:   - Repurposed to be an actual *manager*; Now holds a list of mods with enabled compatibility layers, which can be queried by specialized functions and manually disabled if necessary.   - Now capable of detecting a mod's presence as early as during `PreModsInit`; By default, Rain Meadow and IIC are detected with this new system     - Other mods can still be checked for with the legacy system, but will still have the same constraints of before - Updated class `Utils.ImprovedInputHandler`:   - Now properly catches errors if a keybind fails to be registered, and disables the IIC integration if a key cannot be registered at all - Updated class `Utils.InputHandler`:   - Also added (proper) error-catching for safety and consistency   - Replaced previous technique of try-catching to detect IIC's presence with a simple check from `CompatibilityManager`, which now correctly detects the mod's presence during the `OnEnable` phase.

--- ControlLib/src/Utils/CompatibilityManager.cs
+++ ControlLib/src/Utils/CompatibilityManager.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using System.Linq;
 
 namespace ControlLib.Utils;
@@ -7,12 +8,28 @@ namespace ControlLib.Utils;
 /// </summary>
 public static class CompatibilityManager
 {
+    private static string[] SupportedModIDs { get; } = ["henpemaz_rainmeadow", "improved-input-config"];
+    private static Dictionary<string, bool> ManagedMods { get; } = [];
+
+    /// <summary>
+    /// Enables the manager's hooks for ensuring mod compatibility.
+    /// </summary>
+    public static void ApplyHooks() => On.RainWorld.PreModsInit += PreModsInitHook;
+
+    /// <summary>
+    /// Disables the manager's hooks for mod compatibility.
+    /// </summary>
+    public static void RemoveHooks() => On.RainWorld.PreModsInit -= PreModsInitHook;
+
     /// <summary>
     /// Determines if a given mod is currently enabled.
     /// </summary>
     /// <param name="modID">The ID of the mod to check for.</param>
     /// <returns><c>true</c> if the given mod was found to be enabled, <c>false</c> otherwise.</returns>
-    public static bool IsModEnabled(string modID) => ModManager.ActiveMods.Any(mod => mod.id == modID);
+    public static bool IsModEnabled(string modID) =>
+        ManagedMods.TryGetValue(modID, out bool value)
+            ? value
+            : ModManager.ActiveMods.Any(mod => mod.id == modID);
 
     /// <summary>
     /// Determines if either Improved Input Config or Improved Input Config: Extended are enabled.
@@ -25,4 +42,32 @@ public static class CompatibilityManager
     /// </summary>
     /// <returns><c>true</c> if the mod is enabled, <c>false</c> otherwise.</returns>
     public static bool IsRainMeadowEnabled() => IsModEnabled("henpemaz_rainmeadow");
+
+    /// <summary>
+    /// Overrides the configured compatibility features for a given mod.
+    /// </summary>
+    /// <param name="modID">The identifier of the mod.</param>
+    /// <param name="value">The value to be set.</param>
+    /// <remarks>Warning: Once disabled, a compatibility layer will not be re-enabled until a restart. Use with care.</remarks>
+    public static void SetModCompatibility(string modID, bool value = true) => ManagedMods[modID] = value;
+
+    /// <summary>
+    /// Queries the client's list of enabled mods for toggling compatibility features.
+    /// </summary>
+    private static void PreModsInitHook(On.RainWorld.orig_PreModsInit orig, RainWorld self)
+    {
+        orig.Invoke(self);
+
+        CLLogger.LogDebug("Checking compatibility mods...");
+
+        foreach (string modID in SupportedModIDs)
+        {
+            if (self.options.enabledMods.Contains(modID))
+            {
+                CLLogger.LogInfo($"Adding compatibility layer for {modID}.");
+
+                ManagedMods.Add(modID, true);
+            }
+        }
+    }
 }
